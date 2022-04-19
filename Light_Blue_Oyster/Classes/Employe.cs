7a53b16b7b6489b10035f992a1b52165f305c56d
AMGSex <maksimpetrakov25929@gmail.com>
2022-04-19 08:46:49 +0300

    Добавьте файлы проекта.

---
+++ Light_Blue_Oyster/Classes/Employe.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Light_Blue_Oyster.Classes
+{
+    class Employe
+    {
+        public ObjectId _id { get; set; }
+        public string FIO { get; set; }
+        public string Photo { get; set; }
+        public string WorkName { get; set; }
+        public string Birthday { get; set; }
+        public string Sex { get; set; }
+        public string City { get; set; }
+        public int Passport { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    public static void AddToMongo(Employe employe)
+    {
+        var client = new MongoClient("mongodb://localhost");
+        var database = client.GetDatabase("Maksos");
+        var collection = database.GetCollection<Employe>("Marseks");
+        collection.InsertOneAsync(employe);
+    }
+    }
+
+}
---
+++ Light_Blue_Oyster/Classes/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Light_Blue_Oyster.Classes
+{
+    class Item
+    {
+        public ObjectId _id { get; set; }
+        public string ItemName { get; set; }
+        public double ItemPrice { get; set; }
+        public int ItemDateBuy { get; set; }
+        public int ItemCount  { get; set; }
+
+        public static void AddToMongo(Item item)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Maksos");
+            var collection = database.GetCollection<Item>("Marseks");
+            collection.InsertOneAsync(item);
+        }
+    }
+}
